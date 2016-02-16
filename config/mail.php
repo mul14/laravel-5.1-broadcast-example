277bd6af38438b26bebb1d448853075623c697c2
Mulia Arifandi Nasution <mul14.net@gmail.com>
2016-02-16 20:07:47 +0700

    Update config

--- config/mail.php
+++ config/mail.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'driver' => env('MAIL_DRIVER', 'smtp'),
+    'driver' => env('MAIL_DRIVER', 'log'),
 
     /*
     |--------------------------------------------------------------------------
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'no-reply@example.com', 'name' => 'No Reply'],
 
     /*
     |--------------------------------------------------------------------------
